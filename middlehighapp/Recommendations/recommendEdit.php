8575b88ae178e80c38ceae54a841a96317b64c0b
dwaynebowman <dwayne@woodlawnschool.org>
2014-01-02 16:17:31 -0500

    support for recommendations form

--- middlehighapp/Recommendations/recommendEdit.php
+++ middlehighapp/Recommendations/recommendEdit.php
@@ -1,1 +1,46 @@
 <?php
+/*
+ * Copyright 2013 by Sawyer Bowman and Dwayne Bowman. This program is 
+ * made originally for Woodlawn School, a nonprofit private school in
+ * Davidson, NC. It comes with absolutely no warranty.  You can redistribute 
+ * and/or modify it under the terms of the GNU Public License as published
+ * by the Free Software Foundation (see <http://www.gnu.org/licenses/).
+*/
+
+/*
+ *  implements editing of a new parent statement
+ *	@Sawyer Bowman
+ *	@version 12/28/2013
+ */
+
+include('../applicationHeader.html');
+include('recommendValidate.php');
+	if(!array_key_exists('_form_submit', $_POST)){
+		//the form has not been submitted, so show it
+		include('recommendForm.php');
+	}
+	else {
+		//form has been submitted, so validate it
+		//step 1: errors array lists problems on form submitted
+		$errors = validateForm();
+		//get rid of 0th position
+		array_shift($errors);
+		//step 2: display the errors on the form to fix
+		if($errors){
+			//echo($errors[0]);
+			showErrors($errors);
+			include('recommendForm.php');
+		}
+		else {
+			//This was a successful form submission
+			include('recommendForm2.php');
+		}
+		echo('</div>');
+		echo('</div></body></html>');
+		die();
+	}
+	?>
+</div>
+</body>
+</html>
+
