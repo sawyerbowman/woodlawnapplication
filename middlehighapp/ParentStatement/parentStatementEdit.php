391310d249d29393690817e1a411a125d115f373
dwaynebowman <dwayne@woodlawnschool.org>
2014-01-02 00:14:57 -0500

    change for including the header

--- middlehighapp/ParentStatement/parentStatementEdit.php
+++ middlehighapp/ParentStatement/parentStatementEdit.php
@@ -13,7 +13,7 @@
  *	@version 12/28/2013
  */
 
-include('parentStatementHeader.html');
+include('../applicationHeader.html');
 include('parentStatementValidate.php');
 	if(!array_key_exists('_form_submit', $_POST)){
 		//the form has not been submitted, so show it
