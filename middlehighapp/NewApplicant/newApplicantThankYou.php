e424785ae813973976e401824d1b7c22b693e107
dwaynebowman <dwayne@woodlawnschool.org>
2014-01-01 23:32:03 -0500

    wordsmithing

--- middlehighapp/NewApplicant/newApplicantThankYou.php
+++ middlehighapp/NewApplicant/newApplicantThankYou.php
@@ -18,14 +18,14 @@ include('newApplicantHeader.html');
 ?>
 <div class="success">
 	<ul>
-	<li><strong><font color="green">You have successfully submitted your application!</font></strong></li>
+	<li><strong><font color="green">Your Main Application Form has been sent to our Admissions Office</font></strong></li>
 	</ul></div>
 <br>
 
 <p>Thank you for your submission. Below, you can find other links to complete your application.<br><br>
 
-<a href="http://www.woodlawnschool.org/application/applicant_statement/applicantStatementEdit.php">Student Statement</a> (for middle and upper school applicants only)<br>
-<a href="http://www.woodlawnschool.org/application/parent_statement/parentStatementEdit.php">Parent Statement</a>
+<a href="http://www.woodlawnschool.org/application/applicant_statement/applicantStatementEdit.php">Student Statement Form</a> (for middle and upper school applicants only)<br>
+<a href="http://www.woodlawnschool.org/application/parent_statement/parentStatementEdit.php">Parent Statement Form</a>
 
 <?php 
 include('newApplicantFooter.html');
