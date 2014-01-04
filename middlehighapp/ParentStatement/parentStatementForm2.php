ecc20739bf880e8152e108b9be50584b945d6097
dwaynebowman <dwayne@woodlawnschool.org>
2014-01-04 15:28:32 -0500

    added Select option to popup menu as a default

--- middlehighapp/ParentStatement/parentStatementForm2.php
+++ middlehighapp/ParentStatement/parentStatementForm2.php
@@ -42,7 +42,7 @@ value="<?php echo($_POST['lastname']); ?>">
 
 <select style="display:none;" id="futuregrade" name="futuregrade"><?php
 
-$grades = array("Kindergarten", "1st grade", "2nd grade", "3rd grade", "4th grade", "5th grade", "6th grade", "7th grade", "8th grade", "9th grade", "10th grade", "11th grade", "12th grade");
+$grades = array("Select", "Kindergarten", "1st grade", "2nd grade", "3rd grade", "4th grade", "5th grade", "6th grade", "7th grade", "8th grade", "9th grade", "10th grade", "11th grade", "12th grade");
 foreach ($grades as $grade) {
         echo "<option value'" . $grade . "' ";
         if ($grade == $_POST['futuregrade']){
