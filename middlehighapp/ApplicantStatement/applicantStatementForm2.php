5203994c18581c318a4a8c6a464872af3429f82b
dwaynebowman <dwayne@woodlawnschool.org>
2014-01-04 12:39:19 -0500

    deleted extra comment

--- middlehighapp/ApplicantStatement/applicantStatementForm2.php
+++ middlehighapp/ApplicantStatement/applicantStatementForm2.php
@@ -11,7 +11,7 @@
  *  a form for a new applicant's personal statement for middle or high school at Woodlawn School
  *	@Sawyer Bowman
  *	@version 12/28/2013
- *<input type="hidden" name="next_url" value="http://www.woodlawnschool.org/application/applicant_statement/applicantStatementThankYou.php">
+ *
  */
 
 $datetime = date('M-d-Y-g-i-a-e');
